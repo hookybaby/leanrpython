4263574522bec7eae943df942ea68799bcf3051a
JoshWong <hookybaby@gmail.com>
2018-05-03 11:19:47 +0800

    类学习

--- clsStudy.py
+++ clsStudy.py
@@ -2,6 +2,7 @@
 
 
 class parent:
+    __privateName="Josh" # 私有变量
     def __init__(self):
         print("parent init method")
 
@@ -11,6 +12,7 @@ class parent:
     def __del__(self):
         class_name = self.__class__.__name__
         print(class_name, "destroy")
+        print(self.__privateName)
 
 
 cls = parent()
